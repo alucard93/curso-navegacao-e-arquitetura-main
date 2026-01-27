c638a3aa87b4f6a915136b70928f0a6058586996
Marcus Vinicius Nascimento Ferreira <marcus@agenciaoasis.com.br>
2026-01-26 21:26:17 -0300

    fix: Correct focus timer duration from 25 to 1 minute in TimerType enum

--- lib/app/enums/timer_type.dart
+++ lib/app/enums/timer_type.dart
@@ -1,5 +1,5 @@
 enum TimerType {
-  focus('Modo Foco', 25, 'assets/focus.png'),
+  focus('Modo Foco', 1, 'assets/focus.png'),
   shortBreak('Pausa Curta', 5, 'assets/pause.png'),
   longBreak('Pausa Longa', 15, 'assets/long.png');
 
